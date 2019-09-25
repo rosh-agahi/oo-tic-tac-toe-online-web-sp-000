5fd726992df882c1df1919ab926fd756c2fb3001
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-25 22:06:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,8 +44,8 @@ class TicTacToe
     end
   end
 
-  def valid_move?(board,index)
-    if position_taken?(@board,@index) == false && @index.between?(0,8)
+  def valid_move?(index)
+    if position_taken?(@board,index) == false && index.between?(0,8)
       return true
       else
       return false
