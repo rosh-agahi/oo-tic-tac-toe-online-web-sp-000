d2815969261fc610d90c0354c4a53e0f51e4393f
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-25 22:08:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,8 +28,8 @@ class TicTacToe
     occupied
   end
 
-  def current_player(board)
-  (turn_count(board) % 2 == 0? "X" : "O")
+  def current_player
+  (turn_count(@board) % 2 == 0? "X" : "O")
   end
 
   def move(index, player)
